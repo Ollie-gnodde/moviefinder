e468da35ca69f166ce0218d69e4ea3ae4249d95b
Oliver Gnodde <gnoddeo@gmail.com>
2017-07-03 16:49:54 +0200

    Changed to correct code

--- R/function1.R
+++ R/function1.R
@@ -1,28 +1,19 @@
 
 
 
-i <- "imdb_score"
-a <- "actor_1_name"
-g <- "genres"
-d <- "director_name"
-movie_data <- movie_metadata
+movie_disc <- function(g = NA, i = 0, d = NA, a = NA) {
 
+  if ( is.na(g)) stop ("Please enter a genre for your movie.")
 
-movie_disc <- function(i, g, d, a) {
-
-  if ( missing(i)) stop ("Please enter a minimum imdb rating.")
-
-  if ( missing(g)) stop ("Please enter a genre for your movie.")
-
- if (missing(a) & missing(d)) {
+ if (is.na(a) & is.na(d)) {
    class1 <- (movie_metadata$imdb_score >= i) & grepl(g, movie_metadata$genres)
 
  }
 
-  else if (missing(a)) {
+  else if (is.na(a)) {
    class1 <- (movie_metadata$imdb_score >= i) & grepl(g, movie_metadata$genres) & grepl(d, movie_metadata$director_name)
 
- } else if (missing (d)) {class1 <- (movie_metadata$imdb_score >= i) & grepl(g, movie_metadata$genres) & grepl(a, movie_metadata$actor_1_name)
+ } else if (is.na(d)) {class1 <- (movie_metadata$imdb_score >= i) & grepl(g, movie_metadata$genres) & grepl(a, movie_metadata$actor_1_name)
 
  } else {class1 <- (movie_metadata$imdb_score >= i) & grepl(g, movie_metadata$genres) & grepl(d, movie_metadata$director_name) & grepl(a, movie_metadata$actor_1_name)
 }
@@ -31,5 +22,5 @@ movie_disc <- function(i, g, d, a) {
   movie_metadata$movie_title[ class1 ]
 }
 
-movie_disc( 4, , , )
+
 
