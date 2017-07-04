82e4040a00e7941759a8532477fc5fedb0911cda
Melody Yu <yu.musicssis@gmail.com>
2017-07-04 16:08:44 +0200

    Search function test and documentation

---
+++ tests/testthat/testSearch.R
@@ -0,0 +1,10 @@
+
+context("search")
+
+test_that("Checking if the output is the same", {
+
+  col <- c("movie_title", "director_name", "genres", "actor_1_name", "content_rating")
+  expect_equal(colnames(MovieFinder("Avatar")), col)
+
+})
+
