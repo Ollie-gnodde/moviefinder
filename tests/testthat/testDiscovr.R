be50f73b126b283e293c04b74e42223fc5474bad
Oliver Gnodde <gnoddeo@gmail.com>
2017-07-04 14:15:14 +0200

    More testing

--- tests/testthat/testDiscovr.R
+++ tests/testthat/testDiscovr.R
@@ -18,12 +18,15 @@ test_that("Checking if all the variables are in place", {
   expect_equal(substr(movie_disc("Romance", 7, "Richard Donner", "Marlon Brando"),
   1,
   nchar(movie_disc("Romance", 7, "Richard Donner", ))-1),
-  "Superman")
+  "Superman" )
 
 
-  #expect_equal(movie_disc("Comedy", 5, "Adam Rifkin", ), Detroit Rock City, Shooting\ the\ Warwicks)
-
+ expect_equal(substr(movie_disc("Comedy", 5, "Adam Rifkin",  ),
+                     1,
+                     nchar(movie_disc("Comedy", 5, "Adam Rifkin", ))-1),
+              c("Detroit Rock City", "Shooting the Warwicks"))
 
 
  })
 ?test_that
+?cat
