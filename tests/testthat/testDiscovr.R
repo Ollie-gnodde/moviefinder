600c267059ad71130aca0bfd906dfcd973348ad4
Oliver Gnodde <gnoddeo@gmail.com>
2017-07-04 12:57:57 +0200

    2nd round of teting

--- tests/testthat/testDiscovr.R
+++ tests/testthat/testDiscovr.R
@@ -7,14 +7,23 @@ test_that("Checking if all the variables are in place", {
     nchar(movie_disc("Romance", 7, "Richard Donner", "Marlon Brando"))-1),
     "Superman")
 
-  #expect_equal(movie_disc(""Comedy", 5, "Pete Docter", "Buscemi"), 'Monsters, Inc.')
+  expect_equal(substr(movie_disc("Comedy", 5, "Pete Docter", "Buscemi"),
+                      1,
+                      nchar(movie_disc("Comedy", 5, "Pete Docter", "Buscemi"))-1),
+               "Monsters, Inc.")
+
 })
 
-#test_that("Checking if actor is left out", {
- # expect_equal(movie_disc("Romance", 7, "Richard Donner", ), Superman )
+ test_that("Checking if actor is left out", {
+  expect_equal(substr(movie_disc("Romance", 7, "Richard Donner", "Marlon Brando"),
+  1,
+  nchar(movie_disc("Romance", 7, "Richard Donner", ))-1),
+  "Superman")
+
+
   #expect_equal(movie_disc("Comedy", 5, "Adam Rifkin", ), Detroit Rock City, Shooting\ the\ Warwicks)
 
 
 
-#})
+ })
 ?test_that
