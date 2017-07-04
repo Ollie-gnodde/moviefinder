8402547814f4dfff30ac5b9e4e28f13593a27aa5
Oliver Gnodde <gnoddeo@gmail.com>
2017-07-04 16:21:57 +0200

    New Searchr

--- R/search.R
+++ R/search.R
@@ -4,20 +4,17 @@
 #' Necessary package(s): dplyr
 #'
 #' @param movie_title_search the movie that you want information on
-#' @return Movie title, Director name, Duration, Genre, Main actors name, Content rating, imdb score
+#' @return Movie title, Director name, Genre, Main actors name, Content rating
 #' @examples
 #' MovieFinder("Avatar")
 #' MovieFinder("The Lord of the Rings")
 #' @export
 
-
-
 MovieFinder <- function(movie_title_search){
-
+  library(dplyr)
   if(!any(grepl(movie_title_search, movie_metadata$movie_title))) stop("This movie does not exist in this dataset")
   searchMovie <- filter(movie_metadata, grepl(movie_title_search, movie_metadata$movie_title))
-
-  select(searchMovie, movie_title, director_name, duration, genres, actor_1_name, content_rating, imdb_score)
+  select(searchMovie, movie_title, director_name, genres, actor_1_name, content_rating)
 
 }
 
