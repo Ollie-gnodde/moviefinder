c91f2181939b6207dfee69471ea5ba2431c87294
Oliver Gnodde <gnoddeo@gmail.com>
2017-07-06 18:29:09 +0200

    General documentation

---
+++ R/moviefinder.R
@@ -0,0 +1,13 @@
+#' The Movie Finder function
+#'
+#' This package has 2 functions, Discovr and search.
+#'
+#' The Discovr package helps you to find movies that you wish to watch based on 4 inputs.
+#' These 4 inputs are genre, minimum imdb rating, main actor and director.
+#' The function is called by movie_disc()
+#'
+#' The second function is search.
+#' This function will allow you to see all the important information of a movie
+#' The function is called using MovieFinder
+"_PACKAGE"
+#> [1] "_PACKAGE"
